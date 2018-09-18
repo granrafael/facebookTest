dc7360af014f04b23c9972bf04cf56a55aace67a
Rafael Pascual Morcillo <rpascumo@everis.com>
2018-09-18 10:43:52 +0200

    adsñlfh

--- src/test/java/com/facebook/test/HomeTest.java
+++ src/test/java/com/facebook/test/HomeTest.java
@@ -1,9 +1,13 @@
 package com.facebook.test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
 import com.facebook.pages.HomePage;
 import com.facebook.utils.BeforeAfter;
+import com.facebook.utils.SeleniumUtils;
 
 public class HomeTest extends BeforeAfter{
 	//TODO CREAR LAS VARIABLES DE TODOS LOS ELEMENTOS CAPTURADOS EN EL "PAGE"
@@ -11,5 +15,14 @@ public class HomeTest extends BeforeAfter{
 	private By LabelPassword = HomePage.getLabelPassword();
 	private By ButtonEnviar = HomePage.getEnviarButton();
 	
-
+	
+	@Test
+	public void test01() {
+		WebDriver elemento;
+		elemento = SeleniumUtils.click(driver, LabelEmail);
+		
+	}
+	
+	
+	
 }
